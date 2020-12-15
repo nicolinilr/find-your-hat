ca7b264fd76147af0c41c972f94e863320c9f776
Chemslayer <nicolinilr@gmail.com>
2020-12-15 15:46:57 -0800

    Add game and functionality

---
+++ README.md
@@ -0,0 +1,17 @@
+# Introduction
+Short intro goes here
+
+# Table of Contents
+* [General Info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General Info
+Blah Blah Info
+
+## Technologies
+* Tech 1
+* Tech 2
+
+## Setup
+Blah Blah Setup
--- main.js
+++ main.js
@@ -4,3 +4,122 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+const playerCharacter = '8';
+let gameEnd= false;
+
+class Field {
+    constructor(fieldArray){
+        this.fieldArray = fieldArray;
+    }
+
+    print() {
+        //console.log(this.fieldArray);
+        this.fieldArray.forEach(function(row){
+            console.log(row.join(''));
+        });
+    }
+
+    static generateField(height, width, holePercent){
+        let newField = [];
+        //Generate Holes
+        for (let i= 0; i < height; i++){
+            let currRow = [];
+            for (let j=0; j < width; j++){
+                
+                let isHole = Math.random();
+                if (isHole < holePercent) {
+                    currRow.push(hole);
+                } else {
+                    currRow.push(fieldCharacter);
+                };
+                
+            }
+            newField.push(currRow);
+
+        };
+
+        //Generate Hat
+        let hatX;
+        let hatY;
+        do {
+        hatX = Math.floor(Math.random()*width);
+        hatY = Math.floor(Math.random()*height);
+        } while (hatX ===0 && hatY===0);
+        
+        
+        newField[hatY][hatX] = hat;
+
+        //Generate Player
+
+        newField[0][0] = playerCharacter;
+        return newField;
+
+    }
+
+    cheater () {
+        this.fieldArray.forEach(row =>{
+            row.forEach(element =>{
+                if (element === hole ){
+                    element = fieldCharacter;
+                };
+            });
+        });
+    }
+}
+
+let playerPosX= 0;
+let playerPosY= 0;
+const height = prompt ('How tall? ');
+const width = prompt('How wide? ');
+const holePercent = prompt('What percent of holes (as a decimal)? ');
+const newGame = new Field(Field.generateField(height, width, holePercent));
+newGame.print();
+while (gameEnd===false){
+//Ask for direction
+let dir = prompt('Enter a direction: ');
+//Change direction to uppercase (to standardize)
+dir = dir.toUpperCase();
+//Change current position to previous path character
+
+newGame.fieldArray[playerPosY][playerPosX] = pathCharacter;
+//Determine where player is moving to
+
+if (dir === 'U'){
+    playerPosY -= 1;
+} else if (dir=== 'D') {
+    playerPosY +=1;
+} else if (dir === 'L') {
+    playerPosX -= 1;
+} else if (dir === 'R') {
+    playerPosX += 1;
+} else{
+    console.log ('Wrong input, you lose!')
+    gameEnd=true;
+};
+
+//Checks to see if they lost
+if (newGame.fieldArray[playerPosY][playerPosX] === hole){
+    console.log('You fell in a hole and died!');
+    gameEnd=true;
+};
+
+if (playerPosY < 0 || playerPosY >newGame.fieldArray.length || playerPosX < 0 || playerPosX > newGame.fieldArray[0].length){
+    console.log('You fell off the map into eternity!');
+    gameEnd = true;
+};
+
+//Check to see if they found the hat
+
+if (newGame.fieldArray[playerPosY][playerPosX] === hat){
+    console.log('You found the hat! Winner!');
+    gameEnd = true;
+};
+
+//Move the player pawn
+if (!gameEnd){
+    newGame.fieldArray[playerPosY][playerPosX] = playerCharacter; 
+}
+//Print the map for the next iteration
+newGame.print();
+
+}
